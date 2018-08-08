e389d6212ddca022a5b50b6e8499d8150872c98c
Ivan <ivan_johnrazz@abv.bg>
2018-08-08 08:31:22 +0300

    Car project add country method in Car class

--- CarBrand/src/Car.java
+++ CarBrand/src/Car.java
@@ -1,7 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Car {
     String carBrand;
+    String country;
 
-    Car(String brand){
+    Car(String brand) throws Exception {
         this.carBrand = brand;
+        this.country = setCountry();
     }
+
+    public String setCountry ()throws Exception {
+
+            switch (carBrand) {
+                case "mercedes":
+                case "bwm":
+                case "vw":
+                case "audi":
+                    this.country = " Germany" ;
+                    break;
+                case "peugeot":
+                case "renault":
+                    this.country = " France" ;
+                    break;
+                case "ford":
+                    this.country = " USA" ;
+                    break;
+            }
+        return country;
+        }
 }
--- CarBrand/src/Main.java
+++ CarBrand/src/Main.java
@@ -15,25 +15,7 @@ public class Main {
         }
 
         for (int i = 0; i < obj.size(); i++) {
-            String brand = "";
-            String country = "";
-            brand = obj.get(i).carBrand;
-            switch (brand) {
-                case "mercedes":
-                case "bwm":
-                case "vw":
-                case "audi":
-                    country = " Germany" ;
-                    break;
-                case "peugeot":
-                case "renault":
-                    country = " France" ;
-                    break;
-                case "ford":
-                    country = " USA" ;
-                    break;
-            }
-            System.out.println(brand +country);
+            System.out.println(obj.get(i).carBrand + " " + obj.get(i).country);
         }
     }
 }
